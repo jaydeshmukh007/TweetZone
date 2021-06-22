15c84f2b4aaa055ad0f7eb33abfb75f831204ad8
Jay Deshmukh <jaydeshmukh01@gmail.com>
2021-06-22 22:41:10 +0530

    13th

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const middleware = require('./middleware');
 const path = require('path');
 const bodyParser = require('body-parser');
 const mongoose = require("./database");
+const session = require('express-session');
 
 
 const server = app.listen(port,() => {
@@ -17,6 +18,12 @@ app.set("views","views");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname,"public")));
 
+app.use(session({
+    secret: "Butter Chicken",
+    resave: true,
+    saveUninitialized: false
+}))
+
 // Routes
 const loginRoute = require('./routes/loginRoutes');
 const registerRoute = require('./routes/registerRoutes');
@@ -28,7 +35,8 @@ app.use("/register", registerRoute);
 
 app.get("/", middleware.requireLogin, (req, res, next) => {
     var payload = {
-        pageTitle: "Home"
+        pageTitle: "Home",
+        userLoggedIn: req.session.user
     }
     res.status(200).render('home',payload);
 });
